eab294913c89cd16338bcdd0bc090556054d67c7
tim.s <shenyang@shenyangs-MacBook-Pro.local>
2020-09-12 08:37:53 +0800

    注释语句

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,215 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top">
+      <div class="bg">
+        <div class="sticky-placeholder" ref="pride_tab_fixed">
+          <div class="sticky" :class="titleFixed ? 'isFixed' : ''">
+            <svg-icon iconClass="btc"></svg-icon>
+            <div class="contract">
+              <a href="https://t.me/joinchat/P3KX2BpkAxay1z5QvD3OiA">
+                <svg-icon iconClass="telegram"> </svg-icon
+              ></a>
+              <a
+                href="https://twitter.com/michaelibtc/status/1304364108406349827?s=21"
+              >
+                <svg-icon iconClass="twitter"></svg-icon
+              ></a>
+            </div>
+          </div>
+        </div>
+        <div class="containaer">
+          <div class="t1">UNLOCKING LIQUID CAPITAL<br />FROM STAKED ASSETS</div>
+          <div class="t2">TAP INTO CROSS CHAIN LIQUIDITY AND STAKE FARMING</div>
+          <div class="r3">
+            <a href=""><button>LITEPAPER</button></a>
+          </div>
+        </div>
+        <div id="clock"></div>
+      </div>
+    </div>
+    <footer>
+      <div class="footer-r2"></div>
+      <div class="copyright">© 2020 ibt-blockchain.com</div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Clock } from "./clock.js";
+import axios from "axios";
+import moment from "moment";
+import "./clock.less";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      titleFixed: false
+    };
+  },
+  activated() {
+    this.titleFixed = false;
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    //滚动监听，头部固定
+    handleScroll: function() {
+      let offsetTop = this.$refs.pride_tab_fixed.getBoundingClientRect().top;
+      console.log(offsetTop);
+      this.titleFixed = offsetTop < 0;
+      // some code
+    }
+  },
+  mounted() {
+    this.titleFixed = false;
+    window.addEventListener("scroll", this.handleScroll);
+    axios
+      .get("http://quan.suning.com/getSysTime.do")
+      .then(function(response) {
+        const timeDiff = moment(moment("2020-09-15 00:00:00")).diff(
+          new Date(response.data.sysTime2)
+        );
+        var deadline = new Date(
+          Date.parse(new Date(response.data.sysTime2)) + timeDiff
+        );
+        let clockdDom = new Clock(new Date(deadline), function() {});
+        let clockBox = document.querySelector("#clock");
+        clockBox.appendChild(clockdDom.el);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
-}
+};
 </script>
 
 <style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-size: 14px;
+  min-height: 100vh;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+}
+.top {
+  background-image: url(./assets/bg.jpg);
+  background-position: center;
+  background-size: cover;
+  color: #fff;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* height: 100vh; */
+}
+.contract {
+  position: absolute;
+  right: 100px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.contract svg {
+  cursor: pointer;
+  margin-right: 20px;
+}
+.top .bg {
+  padding-top: 76px;
+  padding-bottom: 135px;
+  background: rgba(0, 63, 96, 0.6);
+}
+.top .sticky-placeholder {
+  height: 103px;
+  margin-bottom: 32px;
+}
+.top .sticky {
+  background: transparent;
+  z-index: 10;
+  color: #fff;
+  padding: 0;
+  -webkit-transition: all ease 0.5s;
+  transition: all ease 0.5s;
+  position: relative;
+}
+.top .sticky.isFixed {
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  background: #fff;
+  z-index: 10;
+  color: #333;
+  padding: 10px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.5);
+}
+.top .sticky.isFixed svg {
+  color: rgb(102, 153, 255);
+  width: 3rem !important;
+  height: 3rem !important;
+}
+.top svg {
+  width: 4rem !important;
+  height: 4rem !important;
+}
+.top .t1 {
+  font-size: 60px;
+  font-weight: 500;
+  position: relative;
+  margin-bottom: 33px;
+}
+.top .t1::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 94px;
+  height: 6px;
+  border-radius: 3px;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    right bottom,
+    from(#0085db),
+    to(#00d1cd)
+  );
+  background: linear-gradient(to bottom right, #0085db 0%, #00d1cd 100%);
+  bottom: -9px;
+  left: 50%;
+  -webkit-transform: translate(-50%, 0);
+  -ms-transform: translate(-50%, 0);
+  transform: translate(-50%, 0);
+}
+.top .t2 {
+  font-size: 22px;
+  font-weight: 300;
+  margin-bottom: 57px;
+}
+.top .r3 button {
+  font-size: 22px;
+  font-weight: 300;
+  color: #3e3e3e;
+  padding: 16px 60px;
+  border-radius: 40px;
+  background: #fff;
+  cursor: pointer;
+  border: none;
+}
+footer {
+  position: relative;
+  background: #1a98a8;
+  color: #fff;
+  padding: 32px;
+}
+@media screen and (max-width: 600px) {
+  footer {
+    padding: 32px;
+  }
+}
+footer .copyright {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  -webkit-transform: translate(-50%, 0);
+  -ms-transform: translate(-50%, 0);
+  transform: translate(-50%, 0);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './icons' // svg 图标
 
 Vue.config.productionTip = false
 
